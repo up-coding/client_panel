e388346c84a8a14e59f305ae45b83d81d9d86fb5
Rahul Sharma <rahulsharma15j@gmail.com.com>
2020-05-13 21:51:49 +0530

    client details

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import store from "./store";
 
 import AppNavbar from "../src/components/layout/AppNavbar";
 import Dashboard from "../src/components/layout/Dashboard";
+import AddClient from "../src/components/clients/AddClient";
+import ClientDetails from "../src/components/clients/ClientDetails";
 import "./App.css";
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Dashboard}></Route>
+              <Route exact path="/client/add" component={AddClient}></Route>
+              <Route exact path="/client/:id" component={ClientDetails}></Route>
             </Switch>
           </div>
         </div>
---
+++ src/components/clients/ClientDetails.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { compose } from "redux";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import Spinner from "../layout/Spinner";
+
+class ClientDetails extends Component {
+  render() {
+    const { client } = this.props;
+    if (client) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/" className="btn btn-link">
+                <i className="fa fa-arrow-circle-left"></i> Back to Dashboard
+              </Link>
+            </div>
+            <div className="col-md-6">
+              <div className="btn-group float-right">
+                <Link to={`/client/edit/${client.id}`} className="btn btn-dark">
+                  Edit
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return <Spinner />;
+    }
+  }
+}
+
+export default compose(
+  firestoreConnect((props) => [
+    {
+      collection: "clients",
+      storeAs: "client",
+      doc: props.match.params.id,
+    },
+  ]),
+  connect(({ firestore: { ordered } }, props) => ({
+    client: ordered.client && ordered.client[0],
+  }))
+)(ClientDetails);
